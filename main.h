1324012675600072b1e795d72119d6a2b581b2a2
Firstname Lastname <email>
2023-05-26 01:48:07 -0700

    1148am

--- main.h
+++ main.h
@@ -12,7 +12,6 @@
 #include <stddef.h>
 
 extern char **environ;
-int space_check(char *lineptr);
 char *_getenv(const char *name);
 size_t _strcspn(const char *str, const char *rej);
 char *_strdup(const char *str);
